7e257e5326359e4d1a9614e8f083280eea311521
Evan Genest <genest.1@osu.edu>
2018-10-16 08:54:34 -0500

    Top half works. Footer commented out, broken.

--- page.elm
+++ page.elm
@@ -4,14 +4,38 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+
+{-
+   view model =
+       div [ class "jumbotron" ]
+           [ h1 [] [ text "Welcome to Dunder Mifflin!" ]
+           , p []
+               [ text "Dunder Mifflin Inc. (stock symbol "
+               , strong [] [ text "DMI" ]
+               , text ") is a micro-cap regional paper and office supply distributor with an emphasis on servicing small-business clients."
+               ]
+           ]
+-}
+
+
 view model =
-    div [ class "jumbotron" ]
-        [ h1 [] [ text "Welcome to Dunder Mifflin!" ]
-        , p []
-            [ text "Dunder Mifflin Inc. (stock symbol "
-            , strong [] [ text "DMI" ]
-            , text ") is a micro-cap regional paper and office supply distributor with an emphasis on servicing small-business clients."
+    div []
+        [ div [ class "container-fluid" ]
+            [ header
+                [ id "site-header" ]
+                [ text "titles and photo" ]
+            , div [ class "content" ] [ text "three blox here in pond div" ]
             ]
+
+        {-
+           , footer
+               [ div [ class "starboard" ]
+                   [ p [ href "https://github.com/atom-box/one-and-one" ] [ text "Github" ]
+                   , p [ href "https://github.com/atom-box/one-and-one" ] [ text "Github" ]
+                   , p [ href "https://github.com/atom-box/one-and-one" ] [ text "Github" ]
+                   ]
+               ]
+        -}
         ]
 
 
